9ad0f243c49c1222d178ae3087c28568d49853eb
Pradhyum R <drpradhyum2016@outlook.com>
2023-05-13 00:54:53 +0000

    Finished features, example still included

--- bit8/bw.py
+++ bit8/bw.py
@@ -3,6 +3,14 @@
 import itertools as it
 import operator as op
 
+def ipad(scr):
+    for x in scr:
+        if len(x) % 2 != 0:
+            x.append(0)
+    if len(scr) % 4 != 0:
+        for _ in range(len(scr) % 4):
+            scr.append([0] * len(scr[0]))
+
 def screen_to_units(scr):
     xsize = 2
     ysize = 4
@@ -19,10 +27,16 @@ def unit_to_braille(unit):
     v = sum(map(op.mul, l, map(pow, it.repeat(2), it.count())))
     return chr(v + 10240)
 
-print(unit_to_braille([[1,0],[1,1],[0,0],[0,0]]))
-
 def render(scr, move_cursor=True):
-    pass
+    ipad(scr)
+    l = screen_to_units(scr)
+    for y in l:
+        for x in y:
+            print(unit_to_braille(x), end='')
+        print()
+
+from random import randint
+render([[randint(0, 1) for _ in range(60)] for _ in range(60)])
 
 def as_str(scr):
     pass
